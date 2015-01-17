8e404c2c2e9b607f1cd00cdd1f68175d3a219645
mwjames <jamesin.hongkong.1@gmail.com>
2015-01-17 11:01:31 +0900

    Move travis folder

--- tests/travis/install-semantic-interlanguage-links.sh
+++ tests/travis/install-semantic-interlanguage-links.sh
@@ -23,7 +23,6 @@ function installToMediaWikiRoot {
 	else
 		composer init --stability dev
 		composer require mediawiki/semantic-interlanguage-links "dev-master" --prefer-source --dev --update-with-dependencies
-		composer require onoi/cache "~1.0" --prefer-source --dev
 
 		cd extensions
 		cd SemanticInterlanguageLinks
