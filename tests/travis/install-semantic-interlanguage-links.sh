3ac96c87fcc8ca1d6100532aed281bc0784ec5bc
gesinn-it <github@gesinn.it>
2021-02-23 10:11:51 +0100

    updated Travis, use CI scripts

--- tests/travis/install-semantic-interlanguage-links.sh
+++ tests/travis/install-semantic-interlanguage-links.sh
@@ -4,71 +4,40 @@ set -ex
 BASE_PATH=$(pwd)
 MW_INSTALL_PATH=$BASE_PATH/../mw
 
-# Run Composer installation from the MW root directory
-function installToMediaWikiRoot {
-	echo -e "Running MW root composer install build on $TRAVIS_BRANCH \n"
+## Install
+echo -e "Running MW root composer install build on $TRAVIS_BRANCH \n"
 
-	cd $MW_INSTALL_PATH
+cd $MW_INSTALL_PATH
 
-	if [ "$PHPUNIT" != "" ]
-	then
-		composer require 'phpunit/phpunit='$PHPUNIT --update-with-dependencies
-	else
-		composer require 'phpunit/phpunit=5.7.*' --update-with-dependencies
-	fi
+if [ "$SIL" != "" ]
+then
+  composer require 'mediawiki/semantic-interlanguage-links='$SIL --update-with-dependencies
+else
+  composer init --stability dev
+  composer require mediawiki/semantic-interlanguage-links "dev-master" --dev --update-with-dependencies
 
-	if [ "$SIL" != "" ]
-	then
-		composer require 'mediawiki/semantic-interlanguage-links='$SIL --update-with-dependencies
-	else
-		composer init --stability dev
-		composer require mediawiki/semantic-interlanguage-links "dev-master" --dev --update-with-dependencies
+  cd extensions
+  cd SemanticInterlanguageLinks
 
-		cd extensions
-		cd SemanticInterlanguageLinks
+  # Pull request number, "false" if it's not a pull request
+  # After the install via composer an additional get fetch is carried out to
+  # update th repository to make sure that the latests code changes are
+  # deployed for testing
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    git fetch origin +refs/pull/"$TRAVIS_PULL_REQUEST"/merge:
+    git checkout -qf FETCH_HEAD
+  else
+    git fetch origin "$TRAVIS_BRANCH"
+    git checkout -qf FETCH_HEAD
+  fi
 
-		# Pull request number, "false" if it's not a pull request
-		# After the install via composer an additional get fetch is carried out to
-		# update th repository to make sure that the latests code changes are
-		# deployed for testing
-		if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-		then
-			git fetch origin +refs/pull/"$TRAVIS_PULL_REQUEST"/merge:
-			git checkout -qf FETCH_HEAD
-		else
-			git fetch origin "$TRAVIS_BRANCH"
-			git checkout -qf FETCH_HEAD
-		fi
+  cd ../..
+fi
 
-		cd ../..
-	fi
+composer dump-autoload
 
-	# Rebuild the class map for added classes during git fetch
-	composer dump-autoload
-}
 
-function updateConfiguration {
-
-	cd $MW_INSTALL_PATH
-
-	# Site language
-	if [ "$SITELANG" != "" ]
-	then
-		echo '$wgLanguageCode = "'$SITELANG'";' >> LocalSettings.php
-	fi
-
-	echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
-	echo 'ini_set("display_errors", 1);' >> LocalSettings.php
-	echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
-	echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
-	echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
-
-	# SMW#1732
-	echo 'wfLoadExtension( "SemanticMediaWiki" );' >> LocalSettings.php
-	echo 'wfLoadExtension( "SemanticInterlanguageLinks" );' >> LocalSettings.php
-
-	php maintenance/update.php --quick
-}
-
-installToMediaWikiRoot
-updateConfiguration
+## Configure
+cd $MW_INSTALL_PATH
+echo 'wfLoadExtension( "SemanticInterlanguageLinks" );' >> LocalSettings.php
