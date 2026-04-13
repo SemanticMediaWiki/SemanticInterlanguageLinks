88fc784fcd42a8efe2758ba6df47da52b83184ea
paladox <paladox@users.noreply.github.com>
2026-04-13 17:17:23 +0100

    Update InterwikiLanguageLink.php

--- src/InterwikiLanguageLink.php
+++ src/InterwikiLanguageLink.php
@@ -2,11 +2,11 @@
 
 namespace SIL;
 
+use MediaWiki\Title\Title;
 use SMW\DataValueFactory;
 use SMW\DIProperty;
 use SMW\DIWikiPage;
 use SMWDIBlob as DIBlob;
-use Title;
 
 /**
  * Represents an object for a manual annotation such as [[en:Foo]] where
